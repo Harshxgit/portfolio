594f351b91f49b201a7955c1dd47e6ece710b92f
Harshxgit <dev.harshitpahade@gmail.com>
2025-02-25 23:27:26 +0530

    built hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+                shine: {
+                  '0%': { 'background-position': '100%' },
+                  '100%': { 'background-position': '-100%' },
+                },
+                marquee: {
+                  '0%': { transform: 'translateX(0%)' },
+                  '100%': { transform: 'translateX(-100%)' },
+                },
+              },
+              animation: {
+                shine: 'shine 5s linear infinite',
+                marquee: 'marquee 25s linear infinite'
+              },
+
+    },
   },
   plugins: [],
 }
